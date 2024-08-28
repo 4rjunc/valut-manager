dfe3e2983846bb85a3bddec21428c4b652fb9f7b
4rjunc <arjunmundamani@gmail.com>
2024-08-28 11:46:37 +0530

    added accounts

--- programs/vault-manager/src/lib.rs
+++ programs/vault-manager/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token, TokenAccount, Transfer};
 
 declare_id!("2eH4VtkkB5X5592hmuQqFQvQ9QKaTEmRZyvQgf9EWyxp");
 
@@ -13,4 +14,60 @@ pub mod vault_manager {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+		init_if_needed,
+		payer = signer,
+		seeds = [b"SSF_ACCOUNT_VAULT"],
+		bump,
+		space = 8
+	)]
+	/// CHECK: Struct field "token_account_owner_pda" is unsafe, but is not documented.
+	token_account_owner_pda: AccountInfo<'info>,
+
+	#[account(mut)]
+	signer: Signer<'info>,
+
+	system_program: Program<'info, System>,
+	token_program:  Program<'info, Token>,
+	rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct Deposit<'info>{
+	#[account(mut,
+		seeds=[b"SSF_ACCOUNT_VAULT"],
+		bump
+	)]
+	token_account_owner_pda: AccountInfo<'info>,
+	#[account(
+		init_if_needed,
+		seeds = [
+			b"SSF_PDA_VAULT".as_ref(),
+			mint_account.key().as_ref()
+		],
+		token::mint      = mint_account,
+		token::authority = token_account_owner_pda,
+		payer            = signer,
+		bump
+	)]
+	pub vault: Account<'info, TokenAccount>,
+
+	#[account(mut)]
+	pub signer: Signer<'info>,
+
+	pub mint_account: Account<'info, Mint>,
+
+	#[account(mut)]
+	pub sender_token_account: Account<'info, TokenAccount>,
+
+	pub token_program:  Program<'info, Token>,
+	pub system_program: Program<'info, System>,
+}
+
+
+#[error_code]
+pub enum VaultError {
+	#[msg("Insufficient Funds in Wallet!")]
+	InsufficientFunds,
+}
--- tests/vault-manager.ts
+++ tests/vault-manager.ts
@@ -1,10 +1,12 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { VaultManager } from "../target/types/vault_manager";
+import { expect } from "chai";
 
 describe("vault-manager", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  // Configure the client to use the local cluster
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.VaultManager as Program<VaultManager>;
 
@@ -12,5 +14,18 @@ describe("vault-manager", () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
+
+    // Fetch the program logs
+    const txLogs = await provider.connection.getTransaction(tx, {
+      commitment: "confirmed",
+    });
+
+    // Check if the logs contain the expected message
+    const expectedMessage = `Program ${program.programId.toBase58()} invoke [1]`;
+    const logContainsMessage = txLogs?.meta?.logMessages?.some((log) =>
+      log.includes(expectedMessage)
+    );
+
+    expect(logContainsMessage).to.be.true;
   });
 });
