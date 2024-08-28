3f0c36524f99ec3b1b621b7a56f9c43dc97ae978
4rjunc <arjunmundamani@gmail.com>
2024-08-28 12:40:30 +0530

    updated .gitignore + added deposit fn + added spltoken packages

--- programs/vault-manager/src/lib.rs
+++ programs/vault-manager/src/lib.rs
@@ -11,6 +11,43 @@ pub mod vault_manager {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn deposit(
+        ctx: Context<Deposit>,
+        amount: u64,
+    ) -> Result<()> {
+        msg!("Amount Deposting: {}", amount); //logs the amount deposited
+        //creates reference to the senders's token amount from ctx.accounts
+        let sender_token_account: &Account<TokenAccount> = &ctx.accounts.sender_token_account;
+
+        //checks the senders has enough tokens to send
+        if sender_token_account.amount < amount{
+            return Err(VaultError::InsufficientFunds.into());
+        }
+
+        // display balance in senders account
+        msg!("Senders account balance: {}", sender_token_account.amount);
+
+        // Creats a transfer instruction for SPL token program
+        let tx_instruct: anchor_spl::token::Transfer = Transfer {
+            from: ctx.accounts.sender_token_account.to_account_info(),
+            to: ctx.accounts.vault.to_account_info(),
+            authority: ctx.accounts.signer.to_account_info(),
+        };  
+
+        //creates a CPI cross platform invocation Context
+        //it combines the token program account with the transfer instruction
+        // READ MORE: https://solana.com/docs/core/cpi
+        let cpi_ctx: CpiContext<anchor_spl::token::Transfer> = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            tx_instruct
+        );
+
+        //executes transfer instruction usign CPI Context
+        // ? shows any errors the may occur during transfer
+        anchor_spl::token::transfer(cpi_ctx, amount)?;
+        Ok(()) // if no erros are thrown is returns Ok(())
+    }
 }
 
 #[derive(Accounts)]
